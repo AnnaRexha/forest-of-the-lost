d0b66447804e8f7a51fb0645531a0cd965f46e70
AnnaRexha <meldori@outlook.com>
2025-04-18 19:00:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -58,41 +58,55 @@ function showAbout() {
 // 3. Galaxy Particle Background
 
 particlesJS("particles-js", {
-  particles: {
-    number: {
-      value: 150,
-      density: { enable: true, value_area: 800 }
+  "particles": {
+    "number": {
+      "value": 160,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
     },
-    color: { value: "#ffffff" },
-    shape: {
-      type: "circle",
-      stroke: { width: 0, color: "#000000" }
+    "color": {
+      "value": "#ffffff"
     },
-    opacity: {
-      value: 0.5,
-      random: true
+    "shape": {
+      "type": "circle"
     },
-    size: {
-      value: 2,
-      random: true
+    "opacity": {
+      "value": 0.5,
+      "random": true
     },
-    move: {
-      enable: true,
-      speed: 0.3,
-      direction: "none",
-      random: false,
-      out_mode: "out"
+    "size": {
+      "value": 1.5,
+      "random": true
+    },
+    "move": {
+      "enable": true,
+      "speed": 0.2,
+      "direction": "none",
+      "random": true,
+      "out_mode": "out"
     }
   },
-  interactivity: {
-    events: {
-      onhover: { enable: true, mode: "repulse" },
-      onclick: { enable: true, mode: "push" }
+  "interactivity": {
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "repulse"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "push"
+      }
     },
-    modes: {
-      repulse: { distance: 50 },
-      push: { particles_nb: 4 }
+    "modes": {
+      "repulse": {
+        "distance": 100
+      },
+      "push": {
+        "particles_nb": 4
+      }
     }
   },
-  retina_detect: true
+  "retina_detect": true
 });
