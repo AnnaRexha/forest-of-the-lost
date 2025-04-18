d04749cd5e5b66b765f8d6b7d7b86ecd6916e7e0
AnnaRexha <meldori@outlook.com>
2025-04-18 16:55:41 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+function addRotationEvents(audioId, imgId) {
+  const audio = document.getElementById(audioId);
+  const img = document.getElementById(imgId);
+
+  audio.addEventListener('play', () => {
+    img.classList.add('rotate');
+  });
+
+  audio.addEventListener('pause', () => {
+    img.classList.remove('rotate');
+  });
+
+  audio.addEventListener('ended', () => {
+    img.classList.remove('rotate');
+  });
+}
+
+addRotationEvents('track1', 'track1-img');
+addRotationEvents('track2', 'track2-img');
+addRotationEvents('track3', 'track3-img');
